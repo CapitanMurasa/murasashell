9c2307b8cd1f88ec3c5748217addc23e497a9cb4
murasa <vladislavkolodiy45@gmail.com>
2026-09-14 17:53:10 +0300

    test

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,5 @@
-console.log("hello!") 
+const inputField = document.getElementById('cmd-input');
+inputField?.addEventListener('input', (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    console.log(target.value);
+});
