88b092244510d131aac16fa1126d95c017a08df3
murasa <vladislavkolodiy45@gmail.com>
2026-09-16 16:05:07 +0300

    some polishing..

--- src/scripts/commands.ts
+++ src/scripts/commands.ts
@@ -20,6 +20,7 @@ export const commands: Record<string, CommandFunction> = {
     // },
     
     socials: () => {
-        return "GitHub: <a href='https://github.com/capitanmurasa'>@capitanmurasa</a>";
+        return "GitHub: <a href='https://github.com/capitanmurasa' target='_blank'>@capitanmurasa</a><br> \
+        X(Twitter): <a href='https://x.com/exedist1' target='_blank' >@exedist1</a>";
     }
 };
