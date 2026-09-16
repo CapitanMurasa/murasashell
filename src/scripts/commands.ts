8c982b48d425b7fabc1cf9eecd5b08d0f5727b68
murasa <vladislavkolodiy45@gmail.com>
2026-09-16 16:06:42 +0300

    md files...

--- src/scripts/commands.ts
+++ src/scripts/commands.ts
@@ -5,20 +5,6 @@ export const commands: Record<string, CommandFunction> = {
         return args.join(" ");
     },
     
-    // man: (args) => {
-    //     if (args.length === 0) return "What manual page do you want?";
-        
-    //     const page = args[0].toLowerCase();
-    //     if (page === 'aboutme') {
-    //         return "man aboutme";
-    //     }
-    //     if (page === 'projects') {
-    //         return "man projects";
-    //     }
-        
-    //     return `No manual entry for ${page}`;
-    // },
-    
     socials: () => {
         return "GitHub: <a href='https://github.com/capitanmurasa' target='_blank'>@capitanmurasa</a><br> \
         X(Twitter): <a href='https://x.com/exedist1' target='_blank' >@exedist1</a>";
